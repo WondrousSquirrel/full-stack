0ae5cb445a2ed1ffcc376f8d2155431768b5f0a3
Alexander <wondroussquirre@gmail.com>
2020-05-29 01:58:14 +0300

    routes and controllers

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ import express from "express";
 
 import morgan from "morgan";
 import { logger } from "./config";
+import router from "./routes";
 
 const app = express();
 
 app.use(morgan("combined", { stream: logger.stream }));
+app.use("/api", router);
 
 export default app;
