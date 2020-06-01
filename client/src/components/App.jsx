70bdc33bc85d63c4845d592b199ca17700e59398
Alexander <alexander@mr.robot>
2020-06-01 15:50:18 +0300

    styles

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "../store";
 
+import "./style.scss";
+
 const App = () => {
   return (
-    <>
+    <div className="centered">
       <Provider store={store}>
-        <h1>All Work Fine</h1>
+        <h1 className="title">All Work Fine</h1>
       </Provider>
-    </>
+    </div>
   );
 };
 
