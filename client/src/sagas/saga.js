e2ad57725e1df5dff2bf409fe9b888ab29fb2499
Alexander <alexander@mr.robot>
2020-06-01 15:42:34 +0300

    added sagas

---
+++ client/src/sagas/saga.js
@@ -0,0 +1,6 @@
+import {} from "redux-saga";
+
+export default function* rootSaga() {
+  // yield all([...Object.values(someSaga)].map(fork))
+  console.log("rootSaga");
+}
--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,19 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+
 import rootReducer from "./reducers";
+import rootSaga from "./sagas/saga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   compose(
-    //applyMiddleware(sagas)
+    applyMiddleware(sagaMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
